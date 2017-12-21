bb6bc08319089344490e98147d5cf1f727a64aa5
Paul Staroch <paulchen@rueckgr.at>
2017-12-21 13:52:23 +0100

    updated plugin check_ftp_space

--- ftp-space.sh
+++ ftp-space.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-echo df | sftp u55033@u55033.your-backup.de 2> /dev/null | tail -n 1 > /tmp/ftp-space
-TOTAL=`cat /tmp/ftp-space |tr -s " " ":"|cut -d ":" -f 2`
-FREE=`cat /tmp/ftp-space |tr -s " " ":"|cut -d ":" -f 4`
+c=0
+while [ true ]; do
+	echo df | sftp u55033@u55033.your-backup.de 2> /dev/null | tail -n 1 > /tmp/ftp-space
+	ERROR=${PIPESTATUS[1]}
+	if [ "$ERROR" -eq "0" ]; then
+		break
+	fi
+	c=$((c+1))
+	if [ "$c" -ge "3" ]; then
+		exit 1
+	fi
+	echo "Error while getting FTP storage space information, retrying in 60 seconds..." 1>&2
+	sleep 60
+	echo "Retrying..." 1>&2
+done
+
+TOTAL=`cat /tmp/ftp-space |tr -s " " ":"|cut -d ":" -f 2|sed -e "s/ //g"`
+FREE=`cat /tmp/ftp-space |tr -s " " ":"|cut -d ":" -f 4|sed -e "s/ //g"`
 mkdir -p /etc/ftp-space
 echo $TOTAL > /etc/ftp-space/total
 echo $FREE > /etc/ftp-space/free
 chown -R nagios:nagios /etc/ftp-space
+rm /tmp/ftp-space
 
