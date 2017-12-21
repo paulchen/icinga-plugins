8f13e067020be523860e9efd0a642f169d11afc9
Paul Staroch <paulchen@rueckgr.at>
2017-12-21 14:07:06 +0100

    updated check_score.pl to use Monitoring::Plugin instead of Nagios::Plugin

--- check_score.pl
+++ check_score.pl
@@ -3,14 +3,14 @@
 use strict;
 use LWP::Simple;
 use JSON qw( decode_json );
-use Nagios::Plugin;
-use Nagios::Plugin::Getopt;
-use Nagios::Plugin::Threshold;
+use Monitoring::Plugin;
+use Monitoring::Plugin::Getopt;
+use Monitoring::Plugin::Threshold;
 use Data::Dumper;
 
-my $plugin = Nagios::Plugin->new(shortname => "check_score");
+my $plugin = Monitoring::Plugin->new(shortname => "check_score");
 
-my $options = Nagios::Plugin::Getopt->new(
+my $options = Monitoring::Plugin::Getopt->new(
     usage => "Usage: %s [OPTIONS]",
     version => "1.0.0",
 );
@@ -34,7 +34,7 @@ $options->arg(
                                         
 $options->getopts();
 
-my $threshold = Nagios::Plugin::Threshold->set_thresholds(
+my $threshold = Monitoring::Plugin::Threshold->set_thresholds(
     warning  => $options->warning . ":",
     critical => $options->critical . ":",
 );
@@ -45,7 +45,7 @@ my $url = sprintf("http://www.pool.ntp.org/scores/%s/json?limit=1",
                   $options->ip);
 my $json = get($url);
 
-$plugin->nagios_die("Unable to load monitoring data: $!") unless defined $json;
+$plugin->plugin_die("Unable to load monitoring data: $!") unless defined $json;
 
 my $result = decode_json($json);
 
@@ -58,4 +58,4 @@ $plugin->add_perfdata(
   threshold => $threshold
 );
 
-$plugin->nagios_exit($code, "current score: " . $result->{'history'}->[0]->{'score'});
+$plugin->plugin_exit($code, "current score: " . $result->{'history'}->[0]->{'score'});
