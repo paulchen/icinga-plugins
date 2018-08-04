564f647cd10fa0e16a93a451fd205f99771c3b10
Paul Staroch <paulchen@rueckgr.at>
2018-08-04 17:34:40 +0200

    added update check for munin-contrib

---
+++ update-checker/lib/github_commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+	exit 1
+fi
+
+# using $1 can be a security issue
+wget -q -O - "https://github.com/$1/commits/master.atom" | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
+
