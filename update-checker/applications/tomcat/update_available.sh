4984105953a3e1b7612eb8aebbcc1b616168a367
Paul Staroch <paulchen@rueckgr.at>
2018-08-15 01:11:07 +0200

    updated checker for tomcat

--- update-checker/applications/tomcat/update_available.sh
+++ update-checker/applications/tomcat/update_available.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-../../lib/github_release.sh apache/tomcat
+wget -q -O - https://tomcat.apache.org/download-90.cgi|grep 9.0.10|head -n 1|sed -e 's/^[^>]*>//;s/<.*//g'
 
--- update-checker/applications/tomcat/update_installed.sh
+++ update-checker/applications/tomcat/update_installed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-grep 'Apache Tomcat Version' /opt/apache-tomcat/RELEASE-NOTES |sed -e "s/[^0-9]//g"
+grep 'Apache Tomcat Version' /opt/apache-tomcat/RELEASE-NOTES |sed -e "s/[^\.0-9]//g"
 
