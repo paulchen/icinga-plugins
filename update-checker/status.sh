3ebf6123ca7da8f295b35d7ed7e256ec74b05ed0
Paul Staroch <paulchen@rueckgr.at>
2018-08-19 11:21:49 +0200

    removed filename as parameter from status.sh

--- update-checker/status.sh
+++ update-checker/status.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-if [ "$1" == "" ]; then
-    echo "Wrong number of arguments"
+DIR=`dirname "$0"`
+cd "$DIR"
+
+STATUSFILE=update.status
+if [ ! -e "$STATUSFILE" ]; then
+    echo "File $STATUSFILE does not exist"
     exit 3
 fi
 
-if [ ! -e "$1" ]; then
-    echo "File $1 does not exist"
+if [ ! -f "$STATUSFILE" ]; then
+    echo "$STATUSFILE is not a file"
     exit 3
 fi
 
-if [ ! -f "$1" ]; then
-    echo "$1 is not a file"
-    exit 3
-fi
-
-if [ ! -r "$1" ]; then
-    echo "$1 cannot be read"
+if [ ! -r "$STATUSFILE" ]; then
+    echo "$STATUSFILE cannot be read"
     exit 3
 fi
 
 # https://stackoverflow.com/questions/2005021/how-can-i-tell-if-a-file-is-older-than-30-minutes-from-bin-sh#2005083
-if test "`find $1 -mmin +200`"; then
-	echo "File $1 is older than 200 minutes"
+if test "`find $STATUSFILE -mmin +200`"; then
+	echo "File $STATUSFILE is older than 200 minutes"
 	exit 3
 fi
 
-readarray status < $1
+readarray status < $STATUSFILE
 
 echo ${status[1]}
 exit ${status[0]}
