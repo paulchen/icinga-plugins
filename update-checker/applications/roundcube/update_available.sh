110222b354733ade9abadba5dab660be21140581
Paul Staroch <paulchen@rueckgr.at>
2019-03-01 17:52:50 +0100

    ignore roundcube versions containing 'RC'

--- update-checker/applications/roundcube/update_available.sh
+++ update-checker/applications/roundcube/update_available.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O - https://roundcube.net/feeds/atom.xml | xpath -e '//entry[not(contains(title/text(),"beta"))][1]/title/text()' 2>/dev/null | sed -e "s/[^\.0-9]//g"
+wget -q -O - https://roundcube.net/feeds/atom.xml | xpath -e '//entry[not(contains(title/text(),"beta")) and not(contains(title/text(),"RC"))][1]/title/text()' 2>/dev/null | sed -e "s/[^\.0-9]//g"
 
