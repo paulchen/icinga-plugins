02122cfa7e59936ad3d794b58252c8ef073fb23d
Paul Staroch <paulchen@rueckgr.at>
2019-07-09 20:32:50 +0200

    introduce timeout for wget

--- update-checker/lib/github_commit.sh
+++ update-checker/lib/github_commit.sh
@@ -5,5 +5,5 @@ if [ "$1" == "" ]; then
 fi
 
 # using $1 can be a security issue
-wget -q -O - "https://github.com/$1/commits/master.atom" | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
+wget -q -O - "https://github.com/$1/commits/master.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
 
--- update-checker/lib/github_release.sh
+++ update-checker/lib/github_release.sh
@@ -8,9 +8,9 @@ fi
 
 if [ "$2" == "legacy" ]; then
 	# some repos (like postfixadmin/postfixadmin) seem to tag their releases in some wrong way
-	wget -q -O - "https://github.com/$1/releases.atom" | xpath -e '//entry[1]/title/text()' 2>/dev/null | sed -e 's/[^0-9]*\([0-9\.]*\).*/\1/g'
+	wget -q -O - "https://github.com/$1/releases.atom" --timeout=15 | xpath -e '//entry[1]/title/text()' 2>/dev/null | sed -e 's/[^0-9]*\([0-9\.]*\).*/\1/g'
 else
 	# stolen from https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
-	curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'
+	wget -q -O - "https://api.github.com/repos/$1/releases/latest" --timeout=15 | grep -Po '"tag_name": "\K.*?(?=")'
 fi
 
