a4ab512c053f80df299302d833742bf5a28bf3e5
Paul Staroch <paulchen@rueckgr.at>
2020-06-03 21:29:40 +0200

    sort Roundcube versions descending

--- update-checker/applications/roundcube/update_available.sh
+++ update-checker/applications/roundcube/update_available.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-../../lib/github_release.sh roundcube/roundcubemail
+wget -q -O - "https://github.com/roundcube/roundcubemail/releases.atom" --timeout=15 | xpath -e '//entry/title/text()' 2>/dev/null|sed -e 's/\[[^\[\]*\]//;s/^\s*//;s/\s*$//'|grep -vi 'release candidate'|sort -r|head -n 1| sed -e 's/[^0-9]*\([0-9\.]*\).*/\1/g'
 
