08e0593c59532b9ae2f6c2fcb53602117e92fd88
Paul Staroch <paulchen@rueckgr.at>
2020-06-04 20:51:27 +0200

    use dokuwiki_updates.php from /opt/admin-tools

--- update-checker/applications/dokuwiki/update_available.sh
+++ update-checker/applications/dokuwiki/update_available.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -q -O - https://rueckgr.at/~paulchen/dokuwiki_updates.php | xpath -e '//item[1]/description/text()' 2>/dev/null |grep "update_check" |sed -e 's/^[^\[]*\[//;s/\].*$//'
+php /opt/admin-tools/dokuwiki_updates.php | xpath -e '//item[1]/description/text()' 2>/dev/null |grep "update_check" |sed -e 's/^[^\[]*\[//;s/\].*$//'
 
 #| sed -E "s/^.*([0-9]{4}-[0-9]{2}-[0-9]{2}[^ ]*)/\1/g;s/\.$//"
 
