26734ccb070eb675dc0f77f1c5fd99fa87c4fbe5
Paul Staroch <paulchen@rueckgr.at>
2020-10-15 16:33:52 +0200

    fix update check for roundcube

--- update-checker/applications/roundcube/update_available.sh
+++ update-checker/applications/roundcube/update_available.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -q -O - "https://github.com/roundcube/roundcubemail/releases.atom" --timeout=15 | xpath -e '//entry/title/text()' 2>/dev/null|sed -e 's/\[[^\[\]*\]//;s/^\s*//;s/\s*$//'|grep -vi 'release candidate'|sort -r|head -n 1| sed -e 's/[^0-9]*\([0-9\.]*\).*/\1/g'
+wget -q -O - "https://github.com/roundcube/roundcubemail/releases.atom" --timeout=15 | grep '<title>' | tail -n +2 | sort -r | head -n 1 | sed -e 's/<[^>]*>//g;s/^\s*//;s/\s*$//;s/[^0-9]*\([0-9\.]*\).*/\1/g'
 
