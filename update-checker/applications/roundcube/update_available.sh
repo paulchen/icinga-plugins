dad776067f366219d1ba96d619a0fffac849a97a
Paul Staroch <paulchen@rueckgr.at>
2021-02-26 23:12:37 +0100

    ignore beta releases

--- update-checker/applications/roundcube/update_available.sh
+++ update-checker/applications/roundcube/update_available.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -q -O - "https://github.com/roundcube/roundcubemail/releases.atom" --timeout=15 | grep '<title>' | tail -n +2 | sort --version-sort -r | head -n 1 | sed -e 's/<[^>]*>//g;s/^\s*//;s/\s*$//;s/[^0-9]*\([0-9\.]*\).*/\1/g'
+wget -q -O - "https://github.com/roundcube/roundcubemail/releases.atom" --timeout=15 | grep '<title>' | grep -vi beta | tail -n +2 | sort --version-sort -r | head -n 1 | sed -e 's/<[^>]*>//g;s/^\s*//;s/\s*$//;s/[^0-9]*\([0-9\.]*\).*/\1/g'
 
