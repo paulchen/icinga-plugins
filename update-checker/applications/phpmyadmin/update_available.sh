22184ca298fa097d0d84f1e5ef8f5d467440017c
Paul Staroch <paulchen@rueckgr.at>
2021-04-05 01:50:40 +0200

    fix check for phpMyAdmin

--- update-checker/applications/phpmyadmin/update_available.sh
+++ update-checker/applications/phpmyadmin/update_available.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -q -O - https://www.phpmyadmin.net/downloads/|grep '<h2>phpMyAdmin'|sed -e 's/^.* //;s/<.*$//'|sort --version-sort -r|head -n 1
+wget -q -O - https://www.phpmyadmin.net/downloads/|grep '<h2[^>]*>phpMyAdmin'|sed -e 's/^.* //;s/<.*$//'|sort --version-sort -r|head -n 1
 
