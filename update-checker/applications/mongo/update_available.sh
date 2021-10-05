2c2b97f4241fb54f3ef55f97a69a3428816bbc6a
Paul Staroch <paulchen@rueckgr.at>
2021-10-05 20:41:04 +0200

    switch check to MongoDB 5.0

--- update-checker/applications/mongo/update_available.sh
+++ update-checker/applications/mongo/update_available.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -q -O - http://repo.mongodb.org/apt/debian/dists/buster/mongodb-org/4.2/main/binary-amd64/Packages|grep -A 1 'Package: mongodb-org$' |grep Version|sed -e 's/Version: //'|sort -V|tail -n 1
+wget -q -O - http://repo.mongodb.org/apt/debian/dists/buster/mongodb-org/5.0/main/binary-amd64/Packages|grep -A 1 'Package: mongodb-org$' |grep Version|sed -e 's/Version: //'|sort -V|tail -n 1
 
 
