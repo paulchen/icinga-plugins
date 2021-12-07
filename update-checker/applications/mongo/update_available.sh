10cea8f5b62c714791977af56b75729f3f1a1bb2
Paul Staroch <paulchen@rueckgr.at>
2021-12-08 00:25:58 +0100

    check Docker Hub instead of the APT repository

--- update-checker/applications/mongo/update_available.sh
+++ update-checker/applications/mongo/update_available.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-wget -q -O - http://repo.mongodb.org/apt/debian/dists/buster/mongodb-org/5.0/main/binary-amd64/Packages|grep -A 1 'Package: mongodb-org$' |grep Version|sed -e 's/Version: //'|sort -V|tail -n 1
-
+../../lib/dockerhub.py mongo
 
