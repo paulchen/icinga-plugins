89cd7c22464370bc9c89c73ed9bdbd8d8e3afd64
Paul Staroch <paulchen@rueckgr.at>
2022-06-27 00:53:35 +0200

    ignore Dokuwiki release candidates

--- update-checker/applications/dokuwiki/update_available.sh
+++ update-checker/applications/dokuwiki/update_available.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-php /opt/admin-tools/dokuwiki_updates.php | xpath -e '//item[1]/description/text()' 2>/dev/null |grep "update_check" |sed -e 's/^[^\[]*\[//;s/\].*$//'
+
+php /opt/admin-tools/dokuwiki_updates.php | xpath -e '//item[not(contains(description/text(), "candidate"))][1]/description/text()' 2>/dev/null|grep "update_check" |sed -e 's/^[^\[]*\[//;s/\].*$//'
 
 #| sed -E "s/^.*([0-9]{4}-[0-9]{2}-[0-9]{2}[^ ]*)/\1/g;s/\.$//"
 
