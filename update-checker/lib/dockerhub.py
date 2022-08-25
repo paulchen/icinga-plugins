9abc30348ff556a01daa16de1107c6ca7aeb9c82
Paul Staroch <paulchen@rueckgr.at>
2022-08-25 20:50:49 +0200

    exclude MongoDB versions that RocketChat does not support

--- update-checker/lib/dockerhub.py
+++ update-checker/lib/dockerhub.py
@@ -7,9 +7,11 @@ url = 'https://registry.hub.docker.com/v1/repositories/' + sys.argv[1] + '/tags'
 
 tags = requests.get(url).json()
 
-regex = re.compile('^[0-9]+\.[0-9]+\.[0-9]$')
+regex = re.compile('^[0-9]+\.[0-9]+\.[0-9]+$')
 names = map(lambda x: x['name'], tags)
 filtered_names = filter(regex.match, names)
+if len(sys.argv) > 2:
+    filtered_names = [n for n in filtered_names if n.startswith(sys.argv[2])]
 
 versions = [LooseVersion(v) for v in filtered_names]
 versions.sort()
