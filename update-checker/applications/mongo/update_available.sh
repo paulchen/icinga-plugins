df05743ce0774c6ae91072d14d700076992199f3
Paul Staroch <paulchen@rueckgr.at>
2022-09-21 18:00:15 +0200

    exit with an error code in case the MongoDB versions supported by Rocket.Chat cannot be retrieved

--- update-checker/applications/mongo/update_available.sh
+++ update-checker/applications/mongo/update_available.sh
@@ -2,5 +2,10 @@
 
 MAJOR_VERSION=`curl https://releases.rocket.chat/latest/info -s | jq '.compatibleMongoVersions | sort_by(.) | reverse | .[0]' -r`
 
+if [ "$MAJOR_VERSION" == "" ]; then
+	echo "Unable to fetch supported MongoDB versions from Rocket.Chat"
+	exit 1
+fi
+
 ../../lib/dockerhub.py library mongo "$MAJOR_VERSION"
 
