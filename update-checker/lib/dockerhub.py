c15e2386edca9c0ec8063148fae4ba71d748e293
Paul Staroch <paulchen@rueckgr.at>
2022-09-24 00:17:41 +0200

    support pagination when calling Docker Hub API

--- update-checker/lib/dockerhub.py
+++ update-checker/lib/dockerhub.py
@@ -6,22 +6,31 @@ from distutils.version import LooseVersion
 namespace = sys.argv[1]
 repository = sys.argv[2]
 
-url = f'https://registry.hub.docker.com/v2/namespaces/{namespace}/repositories/{repository}/tags?page_size=20'
+url = f'https://registry.hub.docker.com/v2/namespaces/{namespace}/repositories/{repository}/tags?page_size=10'
 
-response = requests.get(url)
-if response.status_code >= 300:
-    print(f"Error fetching data from {url}; status code: {response.status_code}; contents:\n{response.text}")
-    sys.exit(1)
+while True:
+    response = requests.get(url)
+    if response.status_code >= 300:
+        print(f"Error fetching data from {url}; status code: {response.status_code}; contents:\n{response.text}")
+        sys.exit(1)
 
-tags = response.json()
+    tags = response.json()
 
-regex = re.compile('^[0-9]+\.[0-9]+\.[0-9]+$')
-names = map(lambda x: x['name'], tags['results'])
-filtered_names = filter(regex.match, names)
-if len(sys.argv) > 3:
-    filtered_names = [n for n in filtered_names if n.startswith(sys.argv[3])]
+    regex = re.compile('^[0-9]+\.[0-9]+\.[0-9]+$')
+    names = [x['name'] for x in  tags['results']]
+    if len(names) == 0:
+        printf('No tags found in response from API')
+        sys.exit(1)
 
-versions = [LooseVersion(v) for v in filtered_names]
-versions.sort()
-print(versions[-1].vstring)
+    filtered_names = filter(regex.match, names)
+    if len(sys.argv) > 3:
+        filtered_names = [n for n in filtered_names if n.startswith(sys.argv[3])]
+
+    versions = [LooseVersion(v) for v in filtered_names]
+    if len(versions) > 0:
+        versions.sort()
+        print(versions[-1].vstring)
+        break
+
+    url = tags['next']
 
