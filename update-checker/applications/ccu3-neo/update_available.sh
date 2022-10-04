ae266aa9167fbc0fbb57d5d1bf97380e4a36e750
Paul Staroch <paulchen@rueckgr.at>
2022-10-04 19:22:24 +0200

    check version of neoserver_ccu3 instead of neoserver

--- update-checker/applications/ccu3-neo/update_available.sh
+++ update-checker/applications/ccu3-neo/update_available.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget "https://www.mediola.com/wp-content/sub-projects/helpdesk/data/downloads.json?_=`date +%s`000" -q -O - | python3 -c "import sys, json; print(json.load(sys.stdin)['de']['software']['neoserver']['current']['info']['version'])"
+wget "https://www.mediola.com/wp-content/sub-projects/helpdesk/data/downloads.json?_=`date +%s`000" -q -O - | python3 -c "import sys, json; print(json.load(sys.stdin)['de']['software']['neoserver_ccu3']['current']['info']['version'])"
 
