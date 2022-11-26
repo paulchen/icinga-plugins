19201aa7e31875509abf5f7b3954f165aa47133d
Paul Staroch <paulchen@rueckgr.at>
2022-11-26 20:38:07 +0100

    always check for tag 'latest' on Docker Hub

--- update-checker/lib/dockerhub.py
+++ update-checker/lib/dockerhub.py
@@ -8,6 +8,21 @@ repository = sys.argv[2]
 
 url = f'https://registry.hub.docker.com/v2/namespaces/{namespace}/repositories/{repository}/tags?page_size=10'
 
+
+url_latest = url + '&name=latest'
+response = requests.get(url)
+if response.status_code >= 300:
+    print(f"Error fetching data from {url}; status code: {response.status_code}; contents:\n{response.text}")
+    sys.exit(1)
+
+tags = response.json()
+latest_tags = [x for x in tags['results'] if x['name'] == 'latest']
+if len(latest_tags) == 0:
+    latest_tag = None
+else:
+    latest_tag = latest_tags[0]['digest']
+
+
 while True:
     response = requests.get(url)
     if response.status_code >= 300:
@@ -16,8 +31,17 @@ while True:
 
     tags = response.json()
 
+    if latest_tag != None:
+        filtered_tags = [x for x in tags['results'] if x['digest'] == latest_tag and x['name'] != 'latest']
+        if len(filtered_tags) == 1:
+            print(filtered_tags[0]['name'])
+            break
+
+        url = tags['next']
+        continue
+
     regex = re.compile('^[0-9]+\.[0-9]+\.[0-9]+$')
-    names = [x['name'] for x in  tags['results']]
+    names = [x['name'] for x in tags['results']]
     if len(names) == 0:
         printf('No tags found in response from API')
         sys.exit(1)
