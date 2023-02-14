354712a3022d65bbac5869c28df9d5d127562d0c
Paul Staroch <paulchen@rueckgr.at>
2023-02-14 21:23:43 +0100

    improve error handling

--- docker/update_images.py
+++ docker/update_images.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-import subprocess, yaml, time, functools
+import subprocess, yaml, time, functools, sys
 
 # https://stackoverflow.com/questions/230751/how-can-i-flush-the-output-of-the-print-function
 print = functools.partial(print, flush=True)
@@ -20,6 +20,7 @@ with open(images_file, 'r') as stream:
 
 print(f'Data read from configuration file: {images}')
 
+error = False
 to_update = set()
 for image in images:
     command = [check_tag_script, image['image']]
@@ -33,8 +34,13 @@ for image in images:
             to_update.add(application)
     elif proc.returncode > 0:
         print(f"Error checking whether {image['image']} is up to date")
+        error = True
     else:
         print(f"Base image {image['image']} is up to date")
+
+if error:
+    print('At least one base image could not be checked')
+    sys.exit(1)
 
 print(f'Applications to update: {to_update}')
 
@@ -43,21 +49,34 @@ for application in to_update:
 
     update_script = Path(__file__).resolve().parent / 'applications' / f'update-{application}.sh'
     if update_script.exists():
-        subprocess.call([update_script])
-        print(f'Rebuilding {application} finished')
+        returncode = subprocess.call([update_script])
+        if returncode > 0:
+            print(f'Error rebuilding {application}')
+            error = True
+        else:
+            print(f'Rebuilding {application} finished')
     else:
         print(f'{application} cannot be rebuilt as script {update_script} cannot be found')
 
-print(f'Running check_tags.sh')
-# https://stackoverflow.com/questions/10177587/redirecting-the-output-of-shell-script-executing-through-python
-command = [check_tags_script, images_file]
-with open(status_file, 'w') as output:
-    subprocess.call(command, stdout=output, shell=False)
+if error:
+    print('At least one error occurred, not running check_tags.sh')
+else:
+    print('Running check_tags.sh')
+    # https://stackoverflow.com/questions/10177587/redirecting-the-output-of-shell-script-executing-through-python
+    command = [check_tags_script, images_file]
+    with open(status_file, 'w') as output:
+        returncode = subprocess.call(command, stdout=output, shell=False)
+        if returncode > 0:
+            print('Error running check_tags.sh')
 
 end_time = time.perf_counter()
 total_time = end_time - start_time
 
 print('Elapsed time: %s seconds' % (round(total_time, 2)))
 
-print(f'Execution completed')
+if error:
+    print('Execution completed with errors')
+    sys.exit(1)
+else:
+    print('Execution successfully completed without any errors')
 
