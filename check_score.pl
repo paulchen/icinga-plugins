2da290eed56726322b39c304e347ec888cfa0b28
Paul Staroch <paulchen@rueckgr.at>
2023-04-08 14:36:04 +0200

    implement filtering by monitor name

--- check_score.pl
+++ check_score.pl
@@ -31,6 +31,12 @@ $options->arg(
     help => "IP address of NTP server",
     required => 1,
 );
+$options->arg(
+    spec => "monitor=s",
+    help => "Name of the monitor to be checked",
+    required => 0,
+    default => "recentmedian",
+);
                                         
 $options->getopts();
 
@@ -41,15 +47,27 @@ my $threshold = Monitoring::Plugin::Threshold->set_thresholds(
 
 alarm $options->timeout;
 
-my $url = sprintf("http://www.pool.ntp.org/scores/%s/json?limit=1&monitor=24",
+my $url = sprintf("http://www.pool.ntp.org/scores/%s/json?limit=200&monitor=*",
                   $options->ip);
 my $json = get($url);
 
 $plugin->plugin_die("Unable to load monitoring data: $!") unless defined $json;
 
+my $monitor_name = $options->monitor;
 my $result = decode_json($json);
+my $score;
+my $monitors = $result->{'monitors'};
+foreach my $item (@$monitors) {
+	if ($item->{'name'} eq $monitor_name) {
+		$score = $item->{'score'};
+		last;
+	}
+}
+if (not length $score) {
+	$plugin->plugin_die("No score for monitor $monitor_name found");
+}
 
-my $code = $threshold->get_status($result->{'history'}->[0]->{'score'});
+my $code = $threshold->get_status($score);
 
 $plugin->add_perfdata(
   label => "score",
