26a79e7ce1263140247969c18f91067fc1e891c3
Paul Staroch <paulchen@rueckgr.at>
2023-12-08 12:54:38 +0100

    correctly handle status code

--- docker/check_versioned.sh
+++ docker/check_versioned.sh
@@ -3,5 +3,9 @@
 DIRECTORY=`dirname $0`
 CHECK_SCRIPT="$DIRECTORY/check_tags.sh"
 
+set -o pipefail
+
 docker ps --format '{{.Image}}' | grep -i ".*:[0-9\.A-Z\-]*$" | grep -v ':latest$' | sort | uniq | "$CHECK_SCRIPT" - | tail -n +2
+
+exit $?
 
