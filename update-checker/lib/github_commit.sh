80fdeaeb3754f833d525288bb4a6e4d9870ac616
Paul Staroch <paulchen@rueckgr.at>
2024-01-08 22:11:03 +0100

    set Accept header when querying Atom feed from GitHub

--- update-checker/lib/github_commit.sh
+++ update-checker/lib/github_commit.sh
@@ -6,8 +6,8 @@ fi
 
 if [ "$2" == "" ]; then
 	# using $1 can be a security issue
-	wget -q -O - "https://github.com/$1/commits/master.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
+	wget -q -O - --header="Accept: text/xml" "https://github.com/$1/commits/master.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
 else
-	wget -q -O - "https://github.com/$1/commits/$2.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
+	wget -q -O - --header="Accept: text/xml" "https://github.com/$1/commits/$2.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
 fi
 
