da24555fac30a53e4259125b570c13d855de9bbb
Paul Staroch <paulchen@rueckgr.at>
2024-03-13 00:12:35 +0100

    do not use Rocket.Chat version tag 'latest'

--- update-checker/applications/mongo/update_available.sh
+++ update-checker/applications/mongo/update_available.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-MAJOR_VERSION=`curl https://releases.rocket.chat/latest/info -s 2> /dev/null | jq '.compatibleMongoVersions | sort_by(.) | reverse | .[0]' -r 2> /dev/null`
+ROCKETCHAT_VERSION=`../rocketchat/update_installed.sh`
+
+MAJOR_VERSION=`curl "https://releases.rocket.chat/$ROCKETCHAT_VERSION/info" -s 2> /dev/null | jq '.compatibleMongoVersions | sort_by(.) | reverse | .[0]' -r 2> /dev/null`
 
 if [ "$MAJOR_VERSION" == "" ]; then
 	echo "Unable to fetch supported MongoDB versions from Rocket.Chat"
