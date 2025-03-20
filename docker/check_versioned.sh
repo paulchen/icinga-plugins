3decac5d136f534f899ca91b50e2e9e7215ca34f
Paul Staroch <paulchen@rueckgr.at>
2025-03-20 17:30:44 +0100

    ignore Docker images with '-custom' in their name (e.g. nextcloud-custom)

--- docker/check_versioned.sh
+++ docker/check_versioned.sh
@@ -5,7 +5,7 @@ CHECK_SCRIPT="$DIRECTORY/check_tags.sh"
 
 set -o pipefail
 
-docker ps --format '{{.Image}}' | grep -i ".*:[0-9\.A-Z\-]*$" | grep -v ':latest$' | sort | uniq | "$CHECK_SCRIPT" - | tail -n +2
+docker ps --format '{{.Image}}' | grep -i ".*:[0-9\.A-Z\-]*$" | grep -v ':latest$' | grep -v '\-custom' | sort | uniq | "$CHECK_SCRIPT" - | tail -n +2
 
 exit $?
 
