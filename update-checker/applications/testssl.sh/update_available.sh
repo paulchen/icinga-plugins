b1971ddf191212233b8a60a180a2e29500ba7bc5
Paul Staroch <paulchen@rueckgr.at>
2026-02-12 22:19:09 +0100

    add update checks for dnmvisser/nagios-testssl and testssl/testssl.sh

---
+++ update-checker/applications/testssl.sh/update_available.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+../../lib/github_commit.sh testssl/testssl.sh
+
+
--- update-checker/lib/github_commit.sh
+++ update-checker/lib/github_commit.sh
@@ -6,7 +6,15 @@ fi
 
 if [ "$2" == "" ]; then
 	# using $1 can be a security issue
-	wget -q -O - --header="Accept: text/xml" "https://github.com/$1/commits/master.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
+	COMMIT=`wget -q -O - --header="Accept: text/xml" "https://github.com/$1/commits/master.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"`
+	if [ "$COMMIT" == "" ]; then
+		DEFAULT_BRANCH=`wget -qO- "https://api.github.com/repos/$1" | jq -r '.default_branch'`
+		if [ "$DEFAULT_BRANCH" != "" ]; then
+			"$0" "$1" "$DEFAULT_BRANCH"
+		fi
+	else
+		echo "$COMMIT"
+	fi
 else
 	wget -q -O - --header="Accept: text/xml" "https://github.com/$1/commits/$2.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
 fi
