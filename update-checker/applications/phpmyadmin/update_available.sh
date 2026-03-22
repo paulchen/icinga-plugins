cdc3a14dbc71474fc1b4ea32b3f54de06466edb9
Paul Staroch <paulchen@rueckgr.at>
2026-03-23 00:51:59 +0100

    fetch phpMyAdmin version from GitHub

--- update-checker/applications/phpmyadmin/update_available.sh
+++ update-checker/applications/phpmyadmin/update_available.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -q -O - https://www.phpmyadmin.net/downloads/|grep '<h2[^>]*>phpMyAdmin'|sed -e 's/^.* //;s/<.*$//'|sort --version-sort -r|head -n 1
+wget -q -O - "https://api.github.com/repos/phpmyadmin/phpmyadmin/releases/latest" --timeout=15|grep -Po '"name": "\K.*?(?=")'
 
