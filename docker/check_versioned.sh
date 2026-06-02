ea4872739bb81982bfc6501991cd061d9901608a
Paul Staroch <paulchen@rueckgr.at>
2026-06-02 22:18:52 +0200

    show number of available updates

--- docker/check_versioned.sh
+++ docker/check_versioned.sh
@@ -5,7 +5,17 @@ CHECK_SCRIPT="$DIRECTORY/check_tags.sh"
 
 set -o pipefail
 
-docker ps --format '{{.Image}}' | grep -i ".*:[0-9\.A-Z\-]*$" | grep -v '\-custom' | sort | uniq | "$CHECK_SCRIPT" - | tail -n +2
+docker ps --format '{{.Image}}' | grep -i ".*:[0-9\.A-Z\-]*$" | grep -v '\-custom' | sort | uniq | "$CHECK_SCRIPT" - | tail -n +2 > /tmp/docker-versioned.tmp
 
-exit $?
+STATUS=$?
+
+COUNT=`grep -c 'update available' /tmp/docker-versioned.tmp`
+
+echo "$COUNT update(s) available"
+
+cat /tmp/docker-versioned.tmp
+
+rm -f /tmp/docker-versioned.tmp
+
+exit $STATUS
 
