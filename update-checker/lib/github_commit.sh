b9a69ee7da9b580f75e5a2dc8468de4200ee2068
Paul Staroch <paulchen@rueckgr.at>
2026-09-10 22:48:50 +0200

    use a GitHub token if available

--- update-checker/lib/github_commit.sh
+++ update-checker/lib/github_commit.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+set -o pipefail
+
 if [ "$1" == "" ]; then
 	exit 1
 fi
 
+CONFIG_FILE=`dirname "$0"`/../.config
+
+if [ -f "$CONFIG_FILE" ]; then
+	. "$CONFIG_FILE"
+fi
+
 if [ "$2" == "" ]; then
 	# using $1 can be a security issue
-	COMMIT=`wget -q -O - --header="Accept: text/xml" "https://github.com/$1/commits/master.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"`
-	if [ "$COMMIT" == "" ]; then
-		DEFAULT_BRANCH=`wget -qO- "https://api.github.com/repos/$1" | jq -r '.default_branch'`
-		if [ "$DEFAULT_BRANCH" != "" ]; then
-			"$0" "$1" "$DEFAULT_BRANCH"
-		fi
-	else
-		echo "$COMMIT"
-	fi
+	URL="https://api.github.com/repos/$1/commits?per_page=1"
 else
-	wget -q -O - --header="Accept: text/xml" "https://github.com/$1/commits/$2.atom" --timeout=15 | xpath -e '//entry[1]/id/text()' 2>/dev/null | sed -e "s/^.*Commit\///g"
+	URL="https://api.github.com/repos/$1/commits?per_page=1&sha=$2"
+fi
+
+if [ "$GITHUB_TOKEN" == "" ]; then
+	wget --timeout=15 --header='Accept: application/vnd.github+json' "$URL" -q -O - | jq -r '.[0].sha' || exit 1
+else
+	wget --timeout=15 --header="Authorization: Bearer $GITHUB_TOKEN" --header='Accept: application/vnd.github+json' "$URL" -q -O - | jq -r '.[0].sha' || exit 1
 fi
 
--- update-checker/lib/github_release.sh
+++ update-checker/lib/github_release.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+set -o pipefail
+
 if [ "$1" == "" ]; then
 	exit 1
 fi
 
-# using $1 can be a security issue
+CONFIG_FILE=`dirname "$0"`/../.config
 
-# stolen from https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
-wget -q -O - "https://api.github.com/repos/$1/releases/latest" --timeout=15 | grep -Po '"tag_name": "\K.*?(?=")'
+if [ -f "$CONFIG_FILE" ]; then
+	. "$CONFIG_FILE"
+fi
+
+# using $1 can be a security issue
+URL="https://api.github.com/repos/$1/releases/latest"
+
+if [ "$GITHUB_TOKEN" == "" ]; then
+	wget --timeout=15 --header='Accept: application/vnd.github+json' "$URL" -q -O - | grep -Po '"tag_name": "\K.*?(?=")' || exit 1
+else
+	wget --timeout=15 --header="Authorization: Bearer $GITHUB_TOKEN" --header='Accept: application/vnd.github+json' "$URL" -q -O - | grep -Po '"tag_name": "\K.*?(?=")' || exit 1
+fi
 
